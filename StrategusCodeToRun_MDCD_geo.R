569fcac68ee1dd06dfab99da4b0ac782bd8abf8f
Mark Velez <mav7014@nyp.org>
2025-06-30 12:59:26 -0400

    Add George's parallel scripts

---
+++ StrategusCodeToRun_MDCD_geo.R
@@ -0,0 +1,68 @@
+# -------------------------------------------------------
+#                     PLEASE READ
+# -------------------------------------------------------
+#
+# You must call "renv::restore()" and follow the prompts
+# to install all of the necessary R libraries to run this
+# project. This is a one-time operation that you must do
+# before running any code.
+#
+# !!! PLEASE RESTART R AFTER RUNNING renv::restore() !!!
+#
+# -------------------------------------------------------
+#renv::restore()
+
+# ENVIRONMENT SETTINGS NEEDED FOR RUNNING Strategus ------------
+Sys.setenv("_JAVA_OPTIONS"="-Xmx4g") # Sets the Java maximum heap space to 4GB
+Sys.setenv("VROOM_THREADS"=1) # Sets the number of threads to 1 to avoid deadlocks on file system
+
+##=========== START OF INPUTS ==========
+cdmDatabaseSchema <- "cdm_mdcd_04172025.dbo" # The database / schema where the data in CDM format live
+workDatabaseSchema <- "cdm_mdcd_04172025.results" # A database /schema where study tables can be written
+outputLocation <- file.path(getwd(), "results")
+databaseName <- "MDCD" # Only used as a folder name for results from the study
+minCellCount <- 5
+cohortTableName <- "sample_study_gh13"
+
+# Create the connection details for your CDM
+# More details on how to do this are found here:
+# https://ohdsi.github.io/DatabaseConnector/reference/createConnectionDetails.html
+connectionDetails <- createConnectionDetails(dbms="sql server", 
+                                             server="ohdsiworkshop.mc.cumc.columbia.edu", 
+                                             extraSettings = 
+                                               "encrypt=true;trustServerCertificate=true")
+
+# You can use this snippet to test your connection
+#conn <- DatabaseConnector::connect(connectionDetails)
+#DatabaseConnector::disconnect(conn)
+
+##=========== END OF INPUTS ==========
+analysisSpecifications <- ParallelLogger::loadSettingsFromJson(
+  fileName = "inst/sampleStudy/sampleStudyAnalysisSpecification.json"
+)
+
+# # fix threads to 1 to correct SCCS fatal error
+# options("strategus.SelfControlledCaseSeriesModule.getDbSccsDataThreads"=1)
+
+executionSettings <- Strategus::createCdmExecutionSettings(
+  workDatabaseSchema = workDatabaseSchema,
+  cdmDatabaseSchema = cdmDatabaseSchema,
+  cohortTableNames = CohortGenerator::getCohortTableNames(cohortTable = cohortTableName),
+  workFolder = file.path(outputLocation, databaseName, "strategusWork"),
+  resultsFolder = file.path(outputLocation, databaseName, "strategusOutput"),
+  minCellCount = minCellCount
+)
+
+if (!dir.exists(file.path(outputLocation, databaseName))) {
+  dir.create(file.path(outputLocation, databaseName), recursive = T)
+}
+ParallelLogger::saveSettingsToJson(
+  object = executionSettings,
+  fileName = file.path(outputLocation, databaseName, "executionSettings.json")
+)
+
+Strategus::execute(
+  analysisSpecifications = analysisSpecifications,
+  executionSettings = executionSettings,
+  connectionDetails = connectionDetails
+)
